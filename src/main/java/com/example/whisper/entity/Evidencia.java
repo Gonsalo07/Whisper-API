3a6ad8af9b430a0842d8147d1e404533beea1ee2
Erick <erickavendao014@gmail.com>
2026-02-02 11:23:39 -0500

    crud de dos tablas, me falta una
    nada

---
+++ src/main/java/com/example/whisper/entity/Evidencia.java
@@ -0,0 +1,34 @@
+package com.example.whisper.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tbl_evidencia")
+public class Evidencia {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "denuncia_id", nullable = false)
+    private Denuncia denunciaId;
+
+    @Column(name = "url")
+    private String url;
+
+    @Column(name = "tipo")
+    private String tipo;
+
+    @Column(name = "creado_en")
+    private Date creadoEn;
+}
