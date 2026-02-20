9ff2a104674beb09c36b891af1c3943df5ed15b9
Erick <erickavendao014@gmail.com>
2026-02-20 15:49:43 -0500

    Casi la cago

--- src/main/java/com/example/whisper/config/WebConfig.java
+++ src/main/java/com/example/whisper/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.whisper.controller;
+package com.example.whisper.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
